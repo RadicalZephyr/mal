070ab76851810dddc6465966289cf496278cbb41
Geoff Shannon <geoffpshannon@gmail.com>
2019-11-14 07:53:56 -0800

    Implement non-terminated list error handling
    Restructure error infrastructure to support rational error handling
    and eventually, good error messages.

--- src/reader2.rs
+++ src/reader2.rs
@@ -2,18 +2,83 @@ use nom::{
     branch::alt,
     bytes::complete::tag,
     character::complete::{char, not_line_ending},
-    combinator::{map, opt, value},
-    error::{context, ParseError, VerboseError},
+    combinator::{cut, map, opt, value},
+    error::{context, ErrorKind as NomErrorKind, ParseError, VerboseError},
     multi::many_till,
     sequence::{preceded, tuple},
-    AsChar, IResult, InputTakeAtPosition,
+    AsChar, Err, IResult, InputTakeAtPosition,
 };
 
 use crate::{Comment, Form};
 
+trait ParseErrorExt<I>: ParseError<I> {
+    #[allow(unused_variables)]
+    fn add_kind(kind: ErrorKind, other: Self) -> Self {
+        other
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Error<'a> {
+    error: VerboseError<&'a str>,
+    kind: ErrorKind,
+}
+
+impl<'a> Error<'a> {
+    fn from_kind(input: &'a str, kind: ErrorKind) -> Self {
+        Error {
+            error: VerboseError::from_error_kind(input, NomErrorKind::Eof),
+            kind,
+        }
+    }
+}
+
+impl<'a> ParseError<&'a str> for Error<'a> {
+    fn from_error_kind(input: &'a str, kind: NomErrorKind) -> Self {
+        Error {
+            error: VerboseError::from_error_kind(input, kind),
+            kind: ErrorKind::Bad,
+        }
+    }
+
+    fn append(input: &'a str, nom_kind: NomErrorKind, other: Self) -> Self {
+        let Error { error, kind } = other;
+        Error {
+            error: VerboseError::append(input, nom_kind, error),
+            kind,
+        }
+    }
+}
+
+impl<'a, I> ParseErrorExt<I> for Error<'a>
+where
+    Error<'a>: ParseError<I>,
+{
+    fn add_kind(kind: ErrorKind, mut other: Self) -> Self {
+        other.kind = kind;
+        other
+    }
+}
+
+fn with_kind<I: Clone, E: ParseErrorExt<I>, F, O>(
+    kind: ErrorKind,
+    f: F,
+) -> impl Fn(I) -> IResult<I, O, E>
+where
+    F: Fn(I) -> IResult<I, O, E>,
+{
+    move |i: I| match f(i) {
+        Ok(o) => Ok(o),
+        Err(Err::Incomplete(i)) => Err(Err::Incomplete(i)),
+        Err(Err::Error(e)) => Err(Err::Error(E::add_kind(kind, e))),
+        Err(Err::Failure(e)) => Err(Err::Failure(E::add_kind(kind, e))),
+    }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub enum Error {
+pub enum ErrorKind {
     Bad,
+    UnterminatedList,
 }
 
 fn whitespacechar(c: char) -> bool {
@@ -23,14 +88,17 @@ fn whitespacechar(c: char) -> bool {
     }
 }
 
-fn whitespace0<T, E: ParseError<T>>(input: T) -> IResult<T, T, E>
+fn whitespace0<T, E: ParseErrorExt<T>>(input: T) -> IResult<T, T, E>
 where
     T: InputTakeAtPosition,
     <T as InputTakeAtPosition>::Item: AsChar,
 {
     input.split_at_position_complete(|item| whitespacechar(item.as_char()))
 }
-fn read_form_comment<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Comment, E> {
+
+fn read_form_comment<'a, E: ParseErrorExt<&'a str>>(
+    input: &'a str,
+) -> IResult<&'a str, Comment, E> {
     context(
         "form_comment",
         map(
@@ -40,7 +108,9 @@ fn read_form_comment<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a 
     )(input)
 }
 
-fn read_line_comment<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Comment, E> {
+fn read_line_comment<'a, E: ParseErrorExt<&'a str>>(
+    input: &'a str,
+) -> IResult<&'a str, Comment, E> {
     context(
         "line_comment",
         map(preceded(tag(";"), not_line_ending), |s: &'a str| {
@@ -49,24 +119,25 @@ fn read_line_comment<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a 
     )(input)
 }
 
-fn read_comment<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Form, E> {
+fn read_comment<'a, E: ParseErrorExt<&'a str>>(input: &'a str) -> IResult<&'a str, Form, E> {
     context(
         "read_comment",
         map(alt((read_form_comment, read_line_comment)), Form::Comment),
     )(input)
 }
 
-fn read_list<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Form, E> {
+fn read_list<'a, E: ParseErrorExt<&'a str>>(input: &'a str) -> IResult<&'a str, Form, E> {
+    let (input, _) = char('(')(input)?;
     context(
         "read_list",
-        map(
-            preceded(char('('), many_till(read_form, char(')'))),
-            |(f, _)| Form::list(f),
+        with_kind(
+            ErrorKind::UnterminatedList,
+            map(cut(many_till(read_form, char(')'))), |(f, _)| Form::list(f)),
         ),
     )(input)
 }
 
-fn read_form<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Form, E> {
+fn read_form<'a, E: ParseErrorExt<&'a str>>(input: &'a str) -> IResult<&'a str, Form, E> {
     context(
         "read_form",
         preceded(
@@ -81,14 +152,19 @@ fn read_form<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, For
     )(input)
 }
 
-fn opt_read_form<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Option<Form>, E> {
+fn opt_read_form<'a, E: ParseErrorExt<&'a str>>(
+    input: &'a str,
+) -> IResult<&'a str, Option<Form>, E> {
     opt(read_form)(input)
 }
 
 pub fn read_str2(input: &str) -> Result<Option<Form>, Error> {
-    match opt_read_form::<VerboseError<&str>>(input) {
+    match opt_read_form::<self::Error>(input) {
         Ok((_, form)) => Ok(form),
-        _ => Err(Error::Bad),
+        Err(e) => match e {
+            Err::Incomplete(_i) => Err(self::Error::from_kind(input, ErrorKind::Bad)),
+            Err::Error(e) | Err::Failure(e) => Err(e),
+        },
     }
 }
 
@@ -147,6 +223,11 @@ mod tests {
         mod list {
             use super::*;
 
+            use nom::error::{
+                ErrorKind::{Alt, ManyTill, Tag},
+                VerboseErrorKind::Nom,
+            };
+
             #[test]
             fn empty() {
                 assert_eq!(read_str2("()"), Ok(Some(Form::empty_list())));
@@ -165,6 +246,19 @@ mod tests {
                 assert_eq!(
                     read_str2("(true nil)"),
                     Ok(Some(Form::list(vec![Form::_true(), Form::nil()])))
+                )
+            }
+
+            #[test]
+            fn unterminated() {
+                assert_eq!(
+                    read_str2("(nil true"),
+                    Err(Error {
+                        error: VerboseError {
+                            errors: vec![("", Nom(Tag)), ("", Nom(Alt)), ("", Nom(ManyTill))]
+                        },
+                        kind: ErrorKind::UnterminatedList,
+                    })
                 )
             }
         }
