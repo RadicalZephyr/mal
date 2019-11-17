abb54c1e4082c4f89af765d423ccc96cabb79985
Geoff Shannon <geoffpshannon@gmail.com>
2019-11-17 03:45:33 -0800

    Rename reader module and file

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod printer;
 pub use printer::pr_str;
 
-mod reader2;
-pub use reader2::{read_str, Error as ReaderError};
+mod reader;
+pub use reader::{read_str, Error as ReaderError};
 
 mod types;
 pub use types::{Atom, Bool, Comment, Complex, Float, Form, Integer, List, Map, Rational, Vector};
